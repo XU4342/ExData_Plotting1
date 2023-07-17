ce87ce705d694e175c8445d6b68efdc51c6c1078
XU4342 <139695562+XU4342@users.noreply.github.com>
2023-07-17 23:18:43 +0200

    Create Plot 1.R

---
+++ Plot 1.R
@@ -0,0 +1,27 @@
+library(dplyr)
+
+Sys.setenv("LANGUAGE" = "En")
+Sys.setlocale("LC_ALL", "English")
+
+data1 <- read.table("household_power_consumption.txt", header = TRUE, sep = ';')
+data1[data1 == "?"] <- NA
+
+data1$Date <- as.Date(data1$Date, format = "%d/%m/%Y")
+data1$Time <- strptime(data1$Time, format = "%H:%M:%S")
+
+start_date <- as.Date("2007-02-01")
+end_date <- as.Date("2007-02-02")
+subdata1 <- data1[data1$Date >= start_date & data1$Date <= end_date, ]
+subdata1$Time <- gsub("2023-07-15", "", subdata1$Time)
+
+datetime_str <- paste(subdata1$Date, subdata1$Time)
+datetime <- as.POSIXlt(datetime_str, format = "%Y-%m-%d %H:%M:%S")
+
+subdata1$Global_active_power <- as.numeric(subdata1$Global_active_power)
+
+file_path <- "plot1.png"
+width <- 480
+height <- 480
+png(file = file_path, width = width, height = height)
+hist(subdata1$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", xlim = c(0, 6))
+dev.off()
